b201cc94ec833869ed8ba68118bdae028c5e2e91
Selim <xpecrise@gmail.com>
2025-07-06 00:40:43 +0300

    initial commit

---
+++ src/content.config.ts
@@ -0,0 +1,21 @@
+import { glob } from "astro/loaders";
+import { z, defineCollection} from "astro:content";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    category: z.array(z.string().optional()),
+  }),
+});
+
+export const collections = { blog };
