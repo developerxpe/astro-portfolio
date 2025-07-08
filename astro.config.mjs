ec290eb473e373cf302d9f3c392314b57e470442
Selim <xpecrise@gmail.com>
2025-07-08 21:18:09 +0300

    search bar & dark-light theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import pagefind from "astro-pagefind";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	image: {
+  image: {
     domains: ["astro.build"],
   },
   devToolbar: {
     enabled: false,
   },
 
-  integrations: [icon()],
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  integrations: [icon(), pagefind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- src/utils/blog.js
+++ src/utils/blog.js
@@ -21,7 +21,7 @@ export async function getSortedPosts() {
 export async function getBlogPagination(paginate) {
   const sortedPosts = await getSortedPosts();
   return paginate(sortedPosts, {
-    pageSize: 2,
+    pageSize: 4,
   });
 }
 
